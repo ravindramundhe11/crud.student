2f3acad5ec07397c7e5fda624a4618cf00c8fc52
ravindramundhe11 <ravindramundhe11@gmail.com>
2025-06-20 23:01:49 +0530

    putmapping

--- Webservices.crud.student/src/main/java/com/controller/Mycontroller.java
+++ Webservices.crud.student/src/main/java/com/controller/Mycontroller.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,11 @@ public class Mycontroller {
 		return t;
 	}
 	
-	
+	@PutMapping("/update/{sid}")
+	public Student updatedata(@RequestBody Student s,@PathVariable int sid)
+	{
+		Student s1=ssi.update(s, sid);
+		return s1;
+	}
 
 }
