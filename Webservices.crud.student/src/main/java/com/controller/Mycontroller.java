1d9c7bdb6e17f200f937e225bde3c865122a6ea1
ravindramundhe11 <ravindramundhe11@gmail.com>
2025-06-20 23:05:00 +0530

    deletemapping

--- Webservices.crud.student/src/main/java/com/controller/Mycontroller.java
+++ Webservices.crud.student/src/main/java/com/controller/Mycontroller.java
@@ -3,6 +3,7 @@ package com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,4 +41,11 @@ public class Mycontroller {
 		return s1;
 	}
 
+	@DeleteMapping("/delete/{sid}")
+	public String deletedata(@PathVariable int sid)
+	{
+		ssi.delete(sid);
+		
+		return "data deleted by id";
+	}
 }
